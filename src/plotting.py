c79540226c7af192eb4252a025a816c412e7695e
Isaac McAuley <mcauleyisaac@gmail.com>
2020-08-19 22:32:04 -0600

    Added labels

--- src/plotting.py
+++ src/plotting.py
@@ -11,7 +11,7 @@ class AnimatedScatter(object):
 
         diff = DiffData('https://www.basketball-reference.com/boxscores/pbp/202008170TOR.html')
         self.full_data = diff.data
-        self.current_data = np.empty([2,2])
+        self.current_data = np.empty([2,2], dtype=float)
         self.current_data[0] = np.array([0,0])
         self.current_data[1] = np.array([0,0])
         self.tick = 0
@@ -19,12 +19,27 @@ class AnimatedScatter(object):
 
 
         # Setup the figure and axes...
-        self.fig, self.ax = plt.subplots()
-        self.ax.axis([0, 2880, -20, 30])
+        self.fig, self.ax = plt.subplots(figsize=(18, 9), dpi=80)
+        self.ax.axis([0, 4, -20, 30])
+        self.ax.set_yticks(range(-20, 31, 10), minor=False)
+        self.ax.set_yticks(range(-20, 31, 5), minor=True)
+        self.ax.grid(which='major', axis='y', linestyle='-', linewidth='1', color='black', alpha=0.1)
+        self.ax.grid(which='minor', axis='y', linestyle='-', linewidth='1', color='black', alpha=0.1)
 
-        self.line, = self.ax.plot([], [], 'o-', lw=2)
+        self.ax.set_xticks(range(0, 5, 1), minor=False)
+        self.ax.grid(which='major', axis='x', linestyle='-', linewidth='1', color='black', alpha=0.3)
+
+        self.ax.set_xlabel('Time (in quaters)')
+        self.ax.set_ylabel('Point Differential\n-Nets        +Raptors')
+        # self.ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('Quater %'))
+
+        # self.fig.(num=None, )
+
+        self.line, = self.ax.plot([], [], 'o-', lw=2, ms=1.5, alpha=0.8)
         # Then setup FuncAnimation.
-        self.ani = animation.FuncAnimation(self.fig, self.update, interval=10, blit=True)
+        self.ani = animation.FuncAnimation(self.fig, self.update, frames=2880, interval=1, blit=True)
+        # self.ani.save('basic_animation.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
+
 
     def setup_plot(self):
         """Initial drawing of the scatter plot."""
@@ -45,11 +60,13 @@ class AnimatedScatter(object):
             current_second = self.tick / self.ticks_per_second 
 
             if(current_second > self.full_data[0][0]):
-                self.current_data = np.concatenate((self.current_data, [self.full_data[0]]), axis=0)
-                self.current_data = np.concatenate((self.current_data, [self.full_data[0]]), axis=0)
+                next_point = np.array([self.full_data[0]], dtype=float)
+                next_point[0][0] /= (60.0*12.0)
+                self.current_data = np.concatenate((self.current_data, next_point), axis=0)
+                self.current_data = np.concatenate((self.current_data, next_point), axis=0)
                 self.full_data = np.delete(self.full_data, 0,0)
             else:
-                self.current_data[-1] = np.array([current_second, self.current_data[-1][1]])
+                self.current_data[-1] = np.array([current_second/(12*60), self.current_data[-1][1]])
 
             yield self.current_data
 
